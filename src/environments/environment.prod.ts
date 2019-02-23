d03e674da917196ca7319712bfda04c042061e78
Shumin <ms.huang.shumin@gmail.com>
2019-02-24 04:25:08 +0800

    edit api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/books' // TODO: To edit once apiUrl (prod) is available
+  apiUrl: 'https://turn-the-page.herokuapp.com/books'
 };
